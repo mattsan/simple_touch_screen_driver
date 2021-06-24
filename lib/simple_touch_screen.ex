16d1e8a1eaa7ee3995cfa591f7c426596594df26
Eiji MATSUMOTO <e.mattsan@gmail.com>
2021-06-24 20:19:19 +0900

    initial commit

---
+++ lib/simple_touch_screen.ex
@@ -0,0 +1,18 @@
+defmodule SimpleTouchScreen do
+  @moduledoc """
+  Documentation for `SimpleTouchScreen`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimpleTouchScreen.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/simple_touch_screen_test.exs
@@ -0,0 +1,8 @@
+defmodule SimpleTouchScreenTest do
+  use ExUnit.Case
+  doctest SimpleTouchScreen
+
+  test "greets the world" do
+    assert SimpleTouchScreen.hello() == :world
+  end
+end
