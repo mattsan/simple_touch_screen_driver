638abab1a9ed5d7e6fc58312a6b5c94e22a506fe
Eiji MATSUMOTO <e.mattsan@gmail.com>
2021-06-24 20:22:11 +0900

    add a driver

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SimpleTouchScreen.MixProject do
 
   def project do
     [
-      app: :simple_touch_screen,
+      app: :simple_touch_screen_driver,
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
@@ -21,6 +21,8 @@ defmodule SimpleTouchScreen.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:scenic, "~> 0.10"},
+      {:input_event, "~> 0.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
