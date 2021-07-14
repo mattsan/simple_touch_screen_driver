daa11022d3dfe123699bf8b35a8b1bc4d7f9d313
Eiji MATSUMOTO <e.mattsan@gmail.com>
2021-07-14 21:43:29 +0900

    Squashed commit of the following:
    commit 74ea1f388c02ed4b57311823c5c0dc2462aa35a8
    Author: Eiji MATSUMOTO <e.mattsan@gmail.com>
    Date:   Wed Jul 14 21:43:16 2021 +0900
    
        fix events of points

--- lib/simple_touch_screen/driver.ex
+++ lib/simple_touch_screen/driver.ex
@@ -13,33 +13,38 @@ defmodule SimpleTouchScreen.Driver do
     {:ok, %{path: path, x: 0, y: 0, viewport: viewport, calibration: opts[:calibration]}}
   end
 
-  def handle_info({:input_event, path, events}, %{path: path} = state) do
-    events
-    |> Enum.reduce(%{touch: nil, x: 0, y: 0}, fn
-      {:ev_abs, :abs_x, x}, event -> %{event | x: x}
-      {:ev_abs, :abs_y, y}, event -> %{event | y: y}
-      {:ev_key, :btn_touch, touch}, event -> %{event | touch: touch}
-      _, event -> event
-    end)
-    |> case do
-      %{touch: 1, x: x, y: y} ->
-        pos = calibrate(state.calibration, {x, y})
-        Logger.debug("down #{inspect(pos)}")
-        Scenic.ViewPort.input(state.viewport, {:cursor_button, {:left, :press, 0, pos}})
+  def handle_info({:input_event, path, events}, %{path: path, x: x, y: y} = state) do
+    state =
+      events
+      |> Enum.reduce(%{touch: nil, x: x, y: y}, fn
+        {:ev_abs, :abs_x, x}, event -> %{event | x: x}
+        {:ev_abs, :abs_y, y}, event -> %{event | y: y}
+        {:ev_key, :btn_touch, touch}, event -> %{event | touch: touch}
+        _, event -> event
+      end)
+      |> case do
+        %{touch: 1, x: x, y: y} ->
+          pos = calibrate(state.calibration, {x, y})
+          Logger.debug("down #{inspect(pos)}")
+          Scenic.ViewPort.input(state.viewport, {:cursor_button, {:left, :press, 0, pos}})
+          %{state | x: x, y: y}
 
-      %{touch: 0, x: x, y: y} ->
-        pos = calibrate(state.calibration, {x, y})
-        Logger.debug("up #{inspect(pos)}")
-        Scenic.ViewPort.input(state.viewport, {:cursor_button, {:left, :release, 0, pos}})
+        %{touch: 0, x: x, y: y} ->
+          pos = calibrate(state.calibration, {x, y})
+          Logger.debug("up #{inspect(pos)}")
+          Scenic.ViewPort.input(state.viewport, {:cursor_button, {:left, :release, 0, pos}})
+          %{state | x: x, y: y}
 
-      %{x: x, y: y} ->
-        pos = calibrate(state.calibration, {x, y})
-        Logger.debug("move #{inspect(pos)}")
-        Scenic.ViewPort.input(state.viewport, {:cursor_pos, pos})
+        %{x: x, y: y} ->
+          pos = calibrate(state.calibration, {x, y})
+          Logger.debug("move #{inspect(pos)}")
+          Scenic.ViewPort.input(state.viewport, {:cursor_pos, pos})
+          %{state | x: x, y: y}
 
-      event ->
-        Logger.debug("unknown event #{inspect(event)}")
-    end
+        event ->
+          Logger.debug("unknown event #{inspect(event)}")
+          state
+      end
 
     {:noreply, state}
   end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleTouchScreen.MixProject do
   def project do
     [
       app: :simple_touch_screen_driver,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
